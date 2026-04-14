2ff30d91d5e57b3008f3e1b6f9b8ad88c9baf725
ava-the-avocado <avpargal@gmail.com>
2026-04-14 20:55:31 +0000

    I added a list that is for easy medium and hard mode. It then when finds out which level is selected and when the player pressed play it selects a random word from that level. It then has _ for each letter of the secret word

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// add javascript here
+//Game State
+let secretWord = ""
+let dashed = []
+
+
+//Levels
+const easyWords = ["Dog", "Cat", "Bat", "Bus", "Age", "Pet", "Ball", "Act"];
+const mediumWords = ["Heart", "Sound", "Cabin", "Daisy", "Karma", "Nacho", "Nails", "Radio"];
+const hardWords = ["Pizzazz", "Quizzes", "Grizzly", "Puzzler", "Zombify", "Squeeze", "Quickly", "Squeaky"];
+
+
+// ____
+
+function startGame() {
+    let level = document.querySelector('input[name="level"]:checked').id;  //finds which level was picked
+
+    if (level === "e"){
+        secretWord = easyWords[Math.floor(Math.random() * easyWords.length)];
+    }
+    else if (level === "m"){
+        secretWord = mediumWords[Math.floor(Math.random() * mediumWords.length)];
+    }
+    else {
+        secretWord = hardWords[Math.floor(Math.random() * hardWords.length)];            //getting a random word from the specific list
+    }
+
+    dashed = [];
+    for(let i = 0; i<secretWord.length; i++){
+        dashed.push("_");
+    }
+document.getElementById("word-display").innerHTML = "<p>Guess the word: " + dashed.join(" ") + "</p>";
+    }
+
+
+document.getElementById("playBtn").onclick = startGame;
